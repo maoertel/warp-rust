2312e712e18b81daf0ae9d12478344c4fbb107fe
Mathias Oertel <mathias.oertel@commercetools.com>
2022-07-28 13:56:14 +0200

    Implement first route

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use warp::Filter;
+use std::i32;
 
-#[derive(Debug)]
+use serde::Serialize;
+use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
+
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -8,7 +11,7 @@ struct Question {
     tags: Option<Vec<String>>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl Question {
@@ -22,17 +25,47 @@ impl Question {
     }
 }
 
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
 #[tokio::main]
 async fn main() {
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+
+    let routes = get_items;
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
+}
+
+async fn get_questions() -> Result<impl Reply, Rejection> {
     let question = Question::new(
-        QuestionId(String::from("me")),
+        QuestionId(String::from("3")),
         "What\'s the fuzz about",
         "Tell me more!",
         Some(&["faq".to_string()]),
     );
 
-    println!("{:#?}", question);
-
-    let hello = warp::get().map(|| format!("Hello world!"));
-    warp::serve(hello).run(([127, 0, 0, 1], 1337)).await
+    match question.id.0.parse::<i32>() {
+        Ok(_) => Ok(warp::reply::json(&question)),
+        Err(_) => Err(warp::reject::custom(InvalidId)),
+    }
 }
