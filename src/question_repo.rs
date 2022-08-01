b765e0a5b105e45912ea8393d8e13dbdca2c0b60
Mathias Oertel <mathias.oertel@commercetools.com>
2022-08-01 22:42:54 +0200

    Add parking_lot to lock access to questions of the repo

--- src/question_repo.rs
+++ src/question_repo.rs
@@ -1,17 +1,18 @@
 use std::{collections::HashMap, sync::Arc};
 
+use parking_lot::RwLock;
 use warp::{Rejection, Reply};
 
 use crate::model::{Pagination, Question, QuestionId};
 
 pub(crate) struct Store {
-  pub(crate) questions: HashMap<QuestionId, Question>,
+  pub(crate) questions: RwLock<HashMap<QuestionId, Question>>,
 }
 
 impl Store {
-  fn init() -> HashMap<QuestionId, Question> {
+  fn init() -> RwLock<HashMap<QuestionId, Question>> {
     let file = include_str!("../questions.json");
-    serde_json::from_str(file).expect("Not able to read question.json.")
+    RwLock::new(serde_json::from_str(file).expect("Not able to read question.json."))
   }
 
   pub fn new() -> Self {
@@ -24,13 +25,14 @@ impl Store {
     params: HashMap<String, String>,
     repo: Arc<Store>,
   ) -> Result<impl Reply, Rejection> {
-    let mut res: &[&Question] = &repo.questions.values().collect::<Vec<_>>();
+    let question_pairs = repo.questions.read();
+    let mut questions: &[&Question] = &question_pairs.values().collect::<Vec<_>>();
 
     if !params.is_empty() {
       let Pagination { start, end } = Pagination::extract_pagination(params)?;
-      res = &res[start..end];
+      questions = &questions[start..end];
     }
 
-    Ok(warp::reply::json(&res))
+    Ok(warp::reply::json(&questions))
   }
 }
